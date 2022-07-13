34b9a442927ea5316503a256d968b7123baac30e
Rafael Pavan <rafael.202010386@unilasalle.edu.br>
2022-07-13 17:50:35 -0300

    post-at-django-admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 from .models import Post
 
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("tittle", "slug", "status", "created_on")
+    list_filter = ("status",)
+    search_fields = ["tittle", "content"]
+    prepopulated_fields = {"slug": ("tittle",)}
+
+admin.site.register(Post, PostAdmin)
