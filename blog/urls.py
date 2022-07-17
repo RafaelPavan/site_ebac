e3b417574bcf2c6aa55831f31ff54988e6e2eafa
Rafael Pavan <rafael.202010386@unilasalle.edu.br>
2022-07-17 15:03:07 -0300

    feat: create_templates_and_views

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from blog import views
 
 urlpatterns = [
-    path('', views.PostView.as_view(), name='home') # views do blog
+    path('', views.PostView.as_view(), name='home'), # views do blog
+    path('<slug:slug>/', views.PostDetail.as_view(), name="post_detail"),
+
 ]
